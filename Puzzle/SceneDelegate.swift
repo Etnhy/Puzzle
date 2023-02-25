2f28e09fcec307cf90246f14ef6aef200ad5308f
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-02-25 15:22:14 +0200

    update

--- Puzzle/SceneDelegate.swift
+++ Puzzle/SceneDelegate.swift
@@ -16,7 +16,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        var window = UIWindow(windowScene: windowScene)
+        self.window = window
+        setupRoot()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -50,3 +53,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 }
 
+extension SceneDelegate {
+    func setupRoot() {
+        let main = MainViewController()
+        let navCon = UINavigationController(rootViewController: main)
+        self.window?.rootViewController = navCon
+        self.window?.makeKeyAndVisible()
+    }
+}
