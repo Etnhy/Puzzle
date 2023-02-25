ce3ad92e5c469e11bc4a9798e0f7733bfcb5646e
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-02-25 16:19:10 +0200

    buttons ref

---
+++ Puzzle/Modules/Menu/MenuButtonsSettings.swift
@@ -0,0 +1,36 @@
+//
+//  MenuButtonsSettings.swift
+//  Puzzle
+//
+//  Created by Евгений Маглена on 25.02.2023.
+//
+
+import Foundation
+
+enum MenuButtons: CaseIterable {
+    case play
+    case gameRules
+    case settings
+    case privacyPolicy
+    
+    var buttonImages: String {
+        switch self {
+        case .play: return "play"
+        case .gameRules: return "game_rules"
+        case .settings: return "settings"
+        case .privacyPolicy: return "privacy_policy"
+        }
+    }
+    var buttonsTitle: String {
+        switch self {
+        case .play: return "Play"
+        case .gameRules: return "Game Rules"
+        case .settings: return "Settings"
+        case .privacyPolicy: return "PrivacyPolicy"
+        }
+    }
+    
+    var backGroundImage: String {
+        return "buttonbackground"
+    }
+}
--- Puzzle/Modules/Menu/MenuViewController.swift
+++ Puzzle/Modules/Menu/MenuViewController.swift
@@ -7,22 +7,55 @@
 
 import UIKit
 
+
 class MenuViewController: MainViewController {
-    
+    lazy var menuButtons: [UIButton] = {
+        var buttons = [UIButton]()
+        MenuButtons.allCases.forEach { button in
+            var butt = UIButton()
+            var config = UIButton.Configuration.tinted()
+            config.background.image = UIImage(named: button.backGroundImage)
+            config.baseForegroundColor = UIColor.white
+            config.attributedTitle = AttributedString(button.buttonsTitle, attributes: AttributeContainer([NSAttributedString.Key.font: UIFont.systemFont(ofSize: 30, weight: .bold)]) )
+            butt.configuration = config
+            buttons.append(butt)
+        }
+        return buttons
+    }()
     
     lazy var buttonStackView: UIStackView = {
-       var stackView = UIStackView()
-        
+        var stackView = UIStackView(arrangedSubviews: menuButtons)
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 12
+        stackView.backgroundColor = .clear
         return stackView
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addSubviews()
     }
-
+    
+    //MARK: - Add Subviews
+    private func addSubviews() {
+        self.view.addSubview(buttonStackView)
+        setupConstraints()
+    }
+    
+    //MARK: - Setup Constraints
+    private func setupConstraints() {
+        buttonStackView.snp.makeConstraints { make in
+            make.top.equalTo(self.backgroundImageCandy.snp.bottom).offset(40)
+            make.leading.equalTo(self.view).offset(16)
+            make.trailing.equalTo(self.view).inset(16)
+            make.bottom.equalTo(self.view).inset(150)
+        }
+    }
 }
+
+
 import SwiftUI
 
 struct ViewControllerProvider: PreviewProvider {
