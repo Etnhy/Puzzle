e9f093316de79ef5c0134498aa453b8fa5dc787b
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-02-25 15:22:29 +0200

    background

--- Puzzle/MainViewController.swift
+++ Puzzle/MainViewController.swift
@@ -6,12 +6,52 @@
 //
 
 import UIKit
+import SnapKit
+
+enum CandyPosition {
+    case defaultPosition
+    case loadingPosition
+}
 
 class MainViewController: UIViewController {
+    
+    let backgroundImage: UIImageView = {
+        var image = UIImageView()
+        image.image = UIImage(named: "background")
+
+        return image
+
+    }()
+    
+    
+    let backgroundImageCandy: UIImageView = {
+        var image = UIImageView()
+        image.image = UIImage(named: "candy")
+        return image
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupBackground()
+    }
+    
+    fileprivate func setupBackground() {
+        view.addSubview( backgroundImage )
+        backgroundImage.addSubview(backgroundImageCandy)
+        setupConstraints()
+    }
+    
+    fileprivate func setupConstraints() {
+        backgroundImage.snp.makeConstraints { make in
+            make.top.bottom.equalTo(self.view)
+            make.leading.trailing.equalTo(self.view)
+        }
+        backgroundImageCandy.snp.makeConstraints { make in
+            make.top.equalTo(backgroundImage.snp.top).offset(70)
+            make.size.equalTo(CGSize(width: 350, height: 233))
+            make.leading.equalTo(self.view).offset(20)
+        }
     }
 
 
